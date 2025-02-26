a97a32f05a73f5a6455fd06f609495ae6189e600
AndreBoi02 <a.puchie2@gmail.com>
2025-02-26 09:14:39 -0600

    Explainatory comments in the scripts

--- script_prueba.cs
+++ script_prueba.cs
@@ -1,7 +1,7 @@
-using UnityEngine
+using UnityEngine //Comentario explicativo 1
 
-public class Boi : MonoBehaviour {
-    void Start() {
-        Debug.Log("Yeet");
-    }
-}
+public class Boi : MonoBehaviour { //Comentario explicativo 2
+    void Start() { //Comentario explicativo 3
+        Debug.Log("Yeet"); //Comentario explicativo 4
+    } //Comentario explicativo 5
+} //Comentario explicativo 6
